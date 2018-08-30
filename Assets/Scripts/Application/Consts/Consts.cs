6dd5080ed1c5eb16ce8dee9f7960749665d53bc0
LiJian <1287053830@qq.com>
2018-08-30 15:44:59 +0800

    添加playerMove到View,为跑道添加碰撞检测器

--- Assets/Scripts/Application/Consts/Consts.cs
+++ Assets/Scripts/Application/Consts/Consts.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public static class Consts
 {
+    //Event名字
     public const string E_ExitScene = "E_ExitScene";
     public const string E_EnterScene = "E_EnterScene";
 
     public const string E_StartUp = "E_StartUp";
 
-
+    //View名字
+    public const string V_PlayerMove = "V_PlayerMove";
 
 }
---
+++ Assets/Scripts/Application/MVC/View/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : View
+{
+    public float speed = 20f;
+
+    private CharacterController m_cc;
+
+    public override string Name{ get{ return Consts.V_PlayerMove;} }
+
+    public override void HandleEvent(string name, object data = null)
+    {
+        
+    }
+
+    private void Awake()
+    {
+        m_cc = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        m_cc.Move(transform.forward * speed * Time.deltaTime);
+    }
+}
