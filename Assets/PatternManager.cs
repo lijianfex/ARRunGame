20bc851cd95355e9bb1595fac484054bc17af22d
LiJian <1287053830@qq.com>
2018-09-06 17:06:17 +0800

    在道路预之下添加放置物体的父物体Item,并创建PatternManager物体资源方案管理类

---
+++ Assets/PatternManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PatternManager : MonoBehaviour {
+
+    public List<Pattern> Patterns = new List<Pattern>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+//一个游戏物体
+[Serializable]
+public class PatterItem
+{
+    public string perfabName;
+    public Vector3 pos;
+}
+
+//一套物体方案
+[Serializable]
+public class Pattern
+{
+    public List<PatterItem> PatterItems = new List<PatterItem>();
+}
